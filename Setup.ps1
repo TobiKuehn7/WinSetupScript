e7ad39baff1782306a70dce6b94900e431187cff
TobiKuehn7 <55159257+TobiKuehn7@users.noreply.github.com>
2021-07-05 22:04:50 +0200

    Create Setup.ps1

---
+++ Setup.ps1
@@ -0,0 +1,29 @@
+# Download  and Install Firefox
+$source = 'https://download.mozilla.org/?product=firefox-latest&os=win64&lang=de'
+Invoke-WebRequest $source -OutFile '.\Firefox.exe'
+.\Firefox.exe
+
+# Download  and Install Thunderbird
+$source = 'https://download.mozilla.org/?product=thunderbird-latest-SSL&os=win64&lang=de'
+Invoke-WebRequest $source -OutFile '.\Thunderbird.exe'
+.\Thunderbird.exe
+
+# Download and Install LibreOffice
+$source = 'https://de.libreoffice.org/donate/dl/win-x86_64/7.1.4/de/LibreOffice_7.1.4_Win_x64.msi'
+$arguments = "/i `"$source`" /quiet"
+Start-Process msiexec.exe -ArgumentList $arguments -Wait
+
+# Download and Install VLC
+$source = 'https://get.videolan.org/vlc/3.0.16/win64/vlc-3.0.16-win64.exe'
+Invoke-WebRequest $source -OutFile '.\VLC.exe'
+.\VLC.exe
+
+ls
+
+# Clear Download Folder
+Write-Host "Press any key to clear the Download Folder..."
+$x = $Host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
+rm .\Firefox.exe
+rm .\Thunderbird.exe
+rm .\LibreOffice.msi
+rm .\VLC.exe
